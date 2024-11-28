d4f510cdd90b62b8580e44697d7da02ed6aae9ad
Stanislav <pavlushinsa18@gmail.com>
2024-11-28 18:23:50 +0300

    RA-03 Выполнена вёрстка кнопок навигации

--- app/src/main/java/com/example/recipesapp/MainActivity.kt
+++ app/src/main/java/com/example/recipesapp/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.example.recipesapp
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.recipesapp.databinding.ActivityMainBinding
+import java.util.zip.Inflater
+
+private var _binding: ActivityMainBinding? = null
+private val binding
+    get () = _binding
+        ?: throw IllegalStateException ("Binding для ActivityMainBinding не должен быть null")
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
