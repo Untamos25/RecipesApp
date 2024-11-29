25689e5e93b05b99b1966c5b3f4b5f7848040035
Stanislav <pavlushinsa18@gmail.com>
2024-11-29 14:08:09 +0300

    RA-03 Создан файл dimens.xml, исправлен отступ для контейнера с кнопками, внесены другие правки

--- app/src/main/java/com/example/recipesapp/MainActivity.kt
+++ app/src/main/java/com/example/recipesapp/MainActivity.kt
@@ -1,18 +1,13 @@
 package com.example.recipesapp
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.recipesapp.databinding.ActivityMainBinding
-import java.util.zip.Inflater
 
 private var _binding: ActivityMainBinding? = null
 private val binding
-    get () = _binding
-        ?: throw IllegalStateException ("Binding для ActivityMainBinding не должен быть null")
+    get() = _binding
+        ?: throw IllegalStateException("Binding для ActivityMainBinding не должен быть null")
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,14 +15,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
