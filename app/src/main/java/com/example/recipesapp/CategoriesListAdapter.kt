c9cef4c531810dc16a850cc7610faeac6ccc65bb
Stanislav <pavlushinsa18@gmail.com>
2024-12-14 21:47:56 +0300

    RA-08 Изменён конструктор класса ViewHolder, добавлен contentDescription

--- app/src/main/java/com/example/recipesapp/CategoriesListAdapter.kt
+++ app/src/main/java/com/example/recipesapp/CategoriesListAdapter.kt
@@ -2,10 +2,7 @@ package com.example.recipesapp
 
 import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recipesapp.databinding.ItemCategoryBinding
 import entity.Category
@@ -14,32 +11,27 @@ import java.io.InputStream
 class CategoriesListAdapter(private val dataSet: List<Category>) :
     RecyclerView.Adapter<CategoriesListAdapter.ViewHolder>() {
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        private val binding: ItemCategoryBinding = ItemCategoryBinding.bind(view)
-
-        val imageView: ImageView = binding.imageCategory
-        val titleTextView: TextView = binding.tvTitle
-        val descriptionTextView: TextView = binding.tvDescription
-    }
+    class ViewHolder(val binding: ItemCategoryBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(viewGroup.context)
-            .inflate(R.layout.item_category, viewGroup, false)
-        return ViewHolder(view)
+        val binding =
+            ItemCategoryBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
         val category = dataSet[position]
-        viewHolder.titleTextView.text = category.title
-        viewHolder.descriptionTextView.text = category.description
-
-        try {
-            val inputStream: InputStream =
-                viewHolder.itemView.context.assets.open(category.imageUrl)
-            val drawable = Drawable.createFromStream(inputStream, null)
-            viewHolder.imageView.setImageDrawable(drawable)
-        } catch (e: Exception) {
-            e.printStackTrace()
+        with(viewHolder.binding) {
+            tvTitle.text = category.title
+            tvDescription.text = category.description
+            try {
+                val inputStream: InputStream = viewHolder.itemView.context.assets.open(category.imageUrl)
+                val drawable = Drawable.createFromStream(inputStream, null)
+                imageCategory.setImageDrawable(drawable)
+                imageCategory.contentDescription = "Изображение категории ${category.title}"
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
     }
 
