54c8f1509f7b01b212d75edd6a2bc806a0c65f20
Stanislav <pavlushinsa18@gmail.com>
2025-01-19 21:11:21 +0300

    RA-12 реализована передача объекта Recipe

--- app/src/main/java/entity/Recipe.kt
+++ app/src/main/java/entity/Recipe.kt
@@ -1,9 +1,13 @@
 package entity
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Recipe(
     val id: Int,
     val title: String,
     val ingredients: List<Ingredient>,
     val method: String,
     val imageUrl: String,
-)
+) : Parcelable
