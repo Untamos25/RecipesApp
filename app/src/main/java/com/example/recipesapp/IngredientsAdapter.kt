c63c4b38b73dc2e91ac9ecf368b5c57ec2c01b0a
Stanislav <pavlushinsa18@gmail.com>
2025-01-21 22:33:07 +0300

    RA-14 Добавлен SeekBar для количества порций и расчёта ингредиентов. Вынесен размер некоторых шрифтов в dimens

--- app/src/main/java/com/example/recipesapp/IngredientsAdapter.kt
+++ app/src/main/java/com/example/recipesapp/IngredientsAdapter.kt
@@ -5,10 +5,13 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recipesapp.databinding.ItemIngredientBinding
 import entity.Ingredient
+import kotlin.math.floor
 
 
 class IngredientsAdapter(private val dataSet: List<Ingredient>) :
     RecyclerView.Adapter<IngredientsAdapter.ViewHolder>() {
+
+    private var quantity = MIN_AMOUNT_OF_PORTIONS
 
     class ViewHolder(val binding: ItemIngredientBinding) : RecyclerView.ViewHolder(binding.root)
 
@@ -20,11 +23,22 @@ class IngredientsAdapter(private val dataSet: List<Ingredient>) :
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
         val ingredient = dataSet[position]
+        val calculatedQuantity = ingredient.quantity.toDouble() * quantity
+
         with(viewHolder.binding) {
             tvIngredientName.text = ingredient.description
-            tvIngredientQuantity.text = "${ingredient.quantity} ${ingredient.unitOfMeasure}"
+            tvIngredientQuantity.text = if (calculatedQuantity == floor(calculatedQuantity)) {
+                "${calculatedQuantity.toInt()} ${ingredient.unitOfMeasure}"
+            } else {
+                "${String.format("%.1f", calculatedQuantity)} ${ingredient.unitOfMeasure}"
+            }
         }
     }
 
     override fun getItemCount() = dataSet.size
+
+    fun updateIngredients(progress: Int) {
+        quantity = progress
+        notifyDataSetChanged()
+    }
 }
