3728c272495b4cd1266e1547fe55aea858d89d33
Stanislav <pavlushinsa18@gmail.com>
2025-01-31 17:34:37 +0300

    RA-22 RecipeFragment подписан на изменения LiveData

--- app/src/main/java/com/example/recipesapp/ui/recipes/recipe/RecipeViewModel.kt
+++ app/src/main/java/com/example/recipesapp/ui/recipes/recipe/RecipeViewModel.kt
@@ -1,16 +1,26 @@
 package com.example.recipesapp.ui.recipes.recipe
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.recipesapp.model.Ingredient
-
-data class RecipeState(
-    val name: String? = null,
-    val image: String?= null,
-    val isFavorite: Boolean = false,
-    val numberOfPortions: Int = MIN_AMOUNT_OF_PORTIONS,
-    val ingredients: List<Ingredient> = emptyList(),
-    val method: List<String> = emptyList(),
-)
+import com.example.recipesapp.model.Recipe
 
 class RecipeViewModel : ViewModel() {
+
+    data class RecipeState(
+        val recipe: Recipe? = null,
+        val numberOfPortions: Int = MIN_AMOUNT_OF_PORTIONS,
+        val isFavorite: Boolean = false,
+    )
+
+    private val _recipeState = MutableLiveData<RecipeState>()
+    val recipeState: LiveData<RecipeState>
+        get() = _recipeState
+
+    init {
+        Log.i("!!!", "RecipeViewModel инициализирована")
+        _recipeState.value = RecipeState(isFavorite = true)
+    }
+
 }
