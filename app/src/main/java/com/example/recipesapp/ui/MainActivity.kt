e8ba647202ee77c0b239604e15d65f197b3e9748
Stanislav <pavlushinsa18@gmail.com>
2025-02-28 23:01:25 +0300

    RA-32 Реализован запрос через URLConnection, произведена десериализация тела ответа сервера

--- app/src/main/java/com/example/recipesapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/recipesapp/ui/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.recipesapp.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import com.example.recipesapp.R
 import com.example.recipesapp.databinding.ActivityMainBinding
+import com.example.recipesapp.model.Category
+import kotlinx.serialization.json.Json
+import java.net.HttpURLConnection
+import java.net.URL
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,13 +26,36 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnCategories.setOnClickListener{
+        val threadName = Thread.currentThread().name
+
+        Log.i("!!!", "Метод onCreate() выполняется на потоке: $threadName")
+
+        val thread = Thread {
+            Log.i("!!!", "Выполняю запрос на потоке: $threadName")
+            val url = URL("https://recipes.androidsprint.ru/api/category")
+            val connection = url.openConnection() as HttpURLConnection
+            connection.connect()
+            val body = connection.inputStream.bufferedReader().readText()
+
+            Log.i("!!!", "responseCode: ${connection.responseCode}")
+            Log.i("!!!", "responseMessage: ${connection.responseMessage}")
+            Log.i("!!!", "Body: $body")
+
+            val categories: List<Category> = Json.decodeFromString(body)
+            Log.i("!!!", "Категории после десериализации: $categories")
+
+        }
+
+        thread.start()
+
+
+        binding.btnCategories.setOnClickListener {
             if (navController.currentDestination?.id != R.id.categoriesListFragment) {
                 navController.navigate(R.id.global_action_to_categoriesListFragment)
             }
         }
 
-        binding.btnFavourites.setOnClickListener{
+        binding.btnFavourites.setOnClickListener {
             if (navController.currentDestination?.id != R.id.favoritesFragment) {
                 navController.navigate(R.id.global_action_to_favoritesFragment)
             }
